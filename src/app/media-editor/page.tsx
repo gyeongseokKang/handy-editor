c267e478480c76bbf48629cdf25494f04a425e1b
handy <kyeungseok.kang@gmail.com>
2024-09-13 23:26:29 +0900

    fix : panel expand error

--- src/app/media-editor/page.tsx
+++ src/app/media-editor/page.tsx
@@ -9,48 +9,49 @@ import {
 } from "@/components/ui/resizable";
 import { useRef, useState } from "react";
 import { GoSidebarExpand } from "react-icons/go";
+import { ImperativePanelHandle } from "react-resizable-panels";
 import MediaEditor from "./component/mediaEditor/MediaEditor";
 import OptionSideBar from "./component/optionSideBar/OptionSideBar";
+
 export default function MediaEditorPage() {
-  const refs = useRef();
+  const ref = useRef<ImperativePanelHandle>();
   const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const handleExpand = () => {
+    ref.current.expand();
+  };
+
   return (
-    <ResizablePanelGroup direction="horizontal" ref={refs} className="relative">
-      <ResizablePanel defaultSize={80} order={1}>
-        <div>
-          <PathBreadcrumb />
-          <MediaEditor />
-        </div>
-      </ResizablePanel>
-      {!isCollapsed ? (
-        <>
-          <ResizableHandle withHandle />
-          <ResizablePanel
-            order={2}
-            defaultValue={20}
-            collapsedSize={0}
-            minSize={20}
-            collapsible
-            onCollapse={() => {
-              setIsCollapsed(true);
-            }}
-            onExpand={() => {
-              setIsCollapsed(false);
-            }}
-          >
-            <OptionSideBar />
-          </ResizablePanel>
-        </>
-      ) : (
-        <Button
-          className="absolute top-0 right-0"
-          onClick={() => {
+    <>
+      <ResizablePanelGroup direction="horizontal" className="relative">
+        <ResizablePanel defaultSize={80}>
+          <div>
+            <PathBreadcrumb />
+            <MediaEditor />
+          </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel
+          ref={ref}
+          defaultValue={20}
+          collapsedSize={0}
+          minSize={15}
+          collapsible
+          onCollapse={() => {
+            setIsCollapsed(true);
+          }}
+          onExpand={() => {
             setIsCollapsed(false);
           }}
         >
-          <GoSidebarExpand />
-        </Button>
-      )}
-    </ResizablePanelGroup>
+          <OptionSideBar />
+        </ResizablePanel>
+        {isCollapsed && (
+          <Button className="absolute top-2 right-2" onClick={handleExpand}>
+            <GoSidebarExpand />
+          </Button>
+        )}
+      </ResizablePanelGroup>
+    </>
   );
 }
